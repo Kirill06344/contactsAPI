475d88fdfb09310b134094bcb70604936928d5b8
stewie <kirill12093@mail.ru>
2023-12-03 21:26:30 +0300

    feat: add PUT and DELETE

---
+++ controllers/contactController.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	"contactsBook/models"
+	u "contactsBook/utils"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var CreateContact = func(w http.ResponseWriter, r *http.Request) {
+
+	user := r.Context().Value("user").(uint)
+	contact := &models.Contact{}
+
+	err := json.NewDecoder(r.Body).Decode(contact)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error!"))
+		return
+	}
+
+	contact.UserId = user
+	resp := contact.CreateContact()
+	u.Respond(w, resp)
+}
+
+var GetContacts = func(w http.ResponseWriter, r *http.Request) {
+
+	id := r.Context().Value("user").(uint)
+	data := models.GetContacts(id)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
+var DeleteContact = func(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("user").(uint)
+	contact := &models.Contact{}
+	err := json.NewDecoder(r.Body).Decode(contact)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error!"))
+		return
+	}
+	contact.UserId = id
+
+	data := models.DeleteContact(contact)
+	if data == nil {
+		u.Respond(w, u.Message(false, "Contact doesn't exist!"))
+		return
+	}
+	resp := u.Message(true, "success")
+	u.Respond(w, resp)
+}
+
+var UpdateContact = func(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("user").(uint)
+	contact := &models.Contact{}
+	err := json.NewDecoder(r.Body).Decode(contact)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error!"))
+		return
+	}
+	if contact.ID == 0 {
+		u.Respond(w, u.Message(false, "Write ID of record to change.You can get it with GET /me/contacts"))
+		return
+	}
+	contact.UserId = id
+
+	data := models.UpdateContact(contact)
+	if data == nil {
+		u.Respond(w, u.Message(false, fmt.Sprintf("You need contact with ID %d", contact.ID)))
+		return
+	}
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
---
+++ models/contact.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	u "contactsBook/utils"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+type Contact struct {
+	gorm.Model
+	Name   string `json:"name"`
+	Phone  string `json:"phone"`
+	UserId uint   `json:"user_id"`
+}
+
+func (contact *Contact) ValidateContact() (map[string]interface{}, bool) {
+
+	if contact.Name == "" {
+		return u.Message(false, "Name cannot be empty!"), false
+	}
+
+	if contact.Phone == "" {
+		return u.Message(false, "Phone number cannot be empty!"), false
+	}
+
+	if contact.UserId <= 0 {
+		return u.Message(false, "User not found!"), false
+	}
+
+	return u.Message(true, "success"), true
+}
+
+func (contact *Contact) CreateContact() map[string]interface{} {
+
+	if response, ok := contact.ValidateContact(); !ok {
+		return response
+	}
+
+	GetDB().Create(contact)
+
+	resp := u.Message(true, "success")
+	resp["contact"] = contact
+	return resp
+}
+
+func GetContact(id uint) *Contact {
+
+	contact := &Contact{}
+	err := GetDB().Table("contacts").Where("id = ?", id).First(contact).Error
+	if err != nil {
+		return nil
+	}
+	return contact
+}
+
+func GetContacts(user uint) []*Contact {
+
+	contactsSlice := make([]*Contact, 0)
+	err := GetDB().Table("contacts").Where("user_id = ?", user).Find(&contactsSlice).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return contactsSlice
+}
+
+func DeleteContact(contact *Contact) *Contact {
+	dbResp := GetDB().Unscoped().Delete(contact)
+	if dbResp.Error != nil {
+		return nil
+	} else if dbResp.RowsAffected < 1 {
+		fmt.Errorf("row with id=%d cannot be deleted because it doesn't exist", contact.ID)
+		return nil
+	}
+
+	return contact
+}
+
+func UpdateContact(contact *Contact) *Contact {
+	db := GetDB()
+	existedContact := &Contact{}
+	db.Where("id = ?", contact.ID).First(existedContact)
+	if existedContact.ID == 0 {
+		return nil
+	}
+	db.Save(&contact)
+	return contact
+}
